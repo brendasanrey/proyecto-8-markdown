e165e0108db1b17f9be441b6dffeadc06a34bcde
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-09-06 17:38:14 -0500

    Agregando comprobación de estatus de los enaces"

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 const path = require('path');
+const marked = require('marked');
 
-const links = [];
+const filterLinks = (array) =>{
+  let links = false;
+  array.forEach(line =>{
+    const openingBracket = line.indexOf('(');
+    const closingBracket = line.indexOf(')');
+    if (openingBracket !== -1 && closingBracket !== -1){
+      if(line.indexOf('http') != -1){
+        links = new Object;
+        links.text = line.slice(line.indexOf('[') + 1, line.indexOf(']'));
+        links.href = line.slice(line.indexOf('http'), line.lastIndexOf(')'));
+      }
+    }
+  })
+  return links;
+}
 
-const checkPath = (url) => {
-  if (path.isAbsolute(url)) {
-    return url;
-  }
-  return path.resolve(url);
-};
 
-const checkLink = () => {
-  links.forEach((link) => {
-    fetch(link.href)
-      .then((response) => {
-        console.log(response.status);
-      }).catch((error) => {
-        console.log(error);
-      });
-  });
-};
-
-fs.readFile('README.md', 'utf-8', (error, data) => {
-  if (error) {
-    console.log('Error', error);
-  } else {
-    let lines = 0;
-    const newString = data.split('\n');
-    newString.forEach((element) => {
-      lines++;
-      const index = element.indexOf('http');
-      if (index !== -1) {
-        const link = element.slice(element.indexOf('(') + 1, element.indexOf(')'));
-        links.push({
-          line: lines,
-          href: link,
-          text: element.slice(element.indexOf('[') + 1, element.indexOf(']')),
-          status: '',
-        });
+const mdLinks = (userPath) => {
+  const mdLinksArrays = [];
+  absolutePath = path.resolve(userPath);
+  fs.readFile(absolutePath, 'utf-8', (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const token = marked.lexer(data);
+      token.forEach(token =>{
+        if(token.type === 'paragraph' || token.type === 'text'){
+          const newArray = token.text.split('\n');
+            const links = filterLinks(newArray, absolutePath);
+            if(links !== false){
+              links.file = absolutePath; 
+              links.status = '';
+              mdLinksArrays.push(links);   
+            }
+          } 
+        }); 
+      checkStatus(mdLinksArrays);
       }
     });
-    console.log(links);
-  }
-});
+}
+
+const checkStatus = links => {
+  links.forEach(link =>{
+    fetch(link.href)
+    .then(response =>{
+      link.status = response.status;
+      console.log(link);
+    });
+  }); 
+};
+
+getArguments = () =>{
+  const listOfArgs = process.argv;
+  mdLinks(listOfArgs[2]);
+}
+
+getArguments();
 
 
